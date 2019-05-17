9057391126a6b713293562bf9fac68c834caa62d
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-17 19:46:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
 def introduction(name)
 end
-def 
-  `"Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
